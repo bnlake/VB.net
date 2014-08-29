a92d2f8237661a5d32df0ebcde8d0e9f37326201
Travis <travis@heliosfx.com>
2014-08-29 10:34:58 -0600

    domain

---
+++ LearningVB.net/FighterManagement/Domain/Fighter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FighterManagement.Domain
+{
+    class Fighter
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public IList<Flight> Flights { get; set; }
+    }
+}
---
+++ LearningVB.net/FighterManagement/Domain/Flight.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FighterManagement.Domain
+{
+    class Flight
+    {
+        public DateTime? Start { get; set; }
+
+        public DateTime? Finish { get; set; }
+    }
+}
